5911da6c307419bc98e532cfd66621976524e88b
Andurian <Devon.Cornwall@web.de>
2024-12-14 20:46:28 +0100

    Setup unit test structure for future days

---
+++ day_23/src/test/kotlin/Day23Test.kt
@@ -0,0 +1,20 @@
+package meow.andurian.aoc2024.day_23
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+import meow.andurian.aoc2024.utils.resourceReader
+
+internal class Day23Test {
+    val lines = resourceReader("/test_input.txt").readLines()
+
+    @Test
+    fun testTask01() {
+        assertTrue(lines.isEmpty())
+    }
+
+    @Test
+    fun testTask02() {
+        assertTrue(lines.isEmpty())
+    }
+}
